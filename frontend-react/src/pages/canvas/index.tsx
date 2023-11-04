81ae3a0bc6cb45f756b19574dec9cda1f37886f3
Shogo_K <shogo.kitada@outlook.jp>
2023-11-04 09:39:02 +0900

    props 更新

--- frontend-react/src/pages/canvas/index.tsx
+++ frontend-react/src/pages/canvas/index.tsx
@@ -4,7 +4,14 @@ import { ThreeJSComponent } from "../../components/mainCanvas/index";
 const Canvas = () => {
   return (
     <Box w="100%" h="100%">
-      <ThreeJSComponent waitUntil={5000} />
+      <ThreeJSComponent waitUntil={5000} flowerData={{
+        flowerText: "TEST",
+        flowerNum: 10,
+        flowerMin: 0.1,
+        flowerMax: 0.6,
+        color1: "rgb(255, 200, 100)",
+        color2: "rgb(0, 200, 100)",
+      }} />
     </Box>
   );
 };
