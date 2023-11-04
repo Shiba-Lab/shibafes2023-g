abab75ec90dc12071d15ffb881c45444cfb66875
Shogo_K <shogo.kitada@outlook.jp>
2023-11-04 13:49:39 +0900

    URL 更新

--- frontend-react/src/components/websocketUtils/utils.ts
+++ frontend-react/src/components/websocketUtils/utils.ts
@@ -1,2 +1,2 @@
 // export const SERVER_URL = 'ws://192.168.0.184:5001/';
-export const SERVER_URL = 'wss://shibafes2023g-f57388251b9d.herokuapp.com//';
+export const SERVER_URL = 'wss://shibafes2023g-f57388251b9d.herokuapp.com/';
